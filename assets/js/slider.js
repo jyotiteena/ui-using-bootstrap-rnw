025ea1e6da357f632ee366a7451424908f369831
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2024-06-22 18:18:52 +0530

    rnw website using bootstrap jquery

---
+++ assets/js/slider.js
@@ -0,0 +1,60 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 4,
+  spaceBetween: 10,
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    280: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
